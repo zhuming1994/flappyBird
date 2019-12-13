867d39b61af550f8c8e14f56e5d6035fde9e7b06
zhuming <123@qq.com>
2019-12-13 17:53:22 +0800

    deylpa结束
    定义资源数组,定义资源加载器,定义变量池,在Main中初始化数据,加载数据

---
+++ js/base/Resources.js
@@ -0,0 +1,9 @@
+//图片资源文件
+export const Resources=[
+    ["background","images/background.png"],
+    ["land","images/land.png"],
+    ["birds","images/birds.png"],
+    ["upPipe","images/pie_up.png"],
+    ["downPipe","images/pie_down.png"],
+    ["startButton","images/start_button.png"]
+];
